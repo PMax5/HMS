80ea1ed5812b0dd6fcc6263d925c55f572cedf6d
Pedro Max <53193337+PMax5@users.noreply.github.com>
2023-02-09 12:09:55 +0000

    feat: Add user Id to config

--- App/data/src/main/java/models/Config.java
+++ App/data/src/main/java/models/Config.java
@@ -2,12 +2,18 @@ package models;
 
 public class Config {
     private final String channelName;
+    private final String hyperledgerUserId;
 
-    public Config(String channelName) {
+    public Config(String channelName, String hyperledgerUserId) {
         this.channelName = channelName;
+        this.hyperledgerUserId = hyperledgerUserId;
     }
 
     public String getChannelName() {
         return this.channelName;
+    }
+
+    public String getHyperledgerUserId() {
+        return hyperledgerUserId;
     }
 }
