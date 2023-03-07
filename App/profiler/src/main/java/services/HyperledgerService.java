45320e9b025ac5db003240a2c4c6189274f1dea0
Pedro Max <53193337+PMax5@users.noreply.github.com>
2023-03-07 10:40:21 +0000

    feat: Implement hyperledger service methods

--- App/profiler/src/main/java/services/HyperledgerService.java
+++ App/profiler/src/main/java/services/HyperledgerService.java
@@ -1,14 +1,19 @@
 package services;
 
+import com.owlike.genson.GenericType;
 import com.owlike.genson.Genson;
-import org.hyperledger.fabric.gateway.Contract;
-import org.hyperledger.fabric.gateway.Gateway;
-import org.hyperledger.fabric.gateway.Wallet;
-import org.hyperledger.fabric.gateway.Wallets;
+import models.Gender;
+import models.Profile;
+import models.ShiftType;
+import org.hyperledger.fabric.gateway.*;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
 
 public class HyperledgerService {
 
@@ -18,6 +23,7 @@ public class HyperledgerService {
     private final static String PROFILER_USER_ID = "profilerUser";
     private final static String PROFILER_CHANNEL = "profiles";
     private final static String PROFILER_CONTRACT = "profiler";
+    private final static String REGISTRY_CONTRACT = "registry";
 
     public HyperledgerService() throws Exception {
         System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
@@ -25,8 +31,8 @@ public class HyperledgerService {
         this.genson = new Genson();
     }
 
-    public Contract getContract(Gateway gateway) {
-        return gateway.getNetwork(PROFILER_CHANNEL).getContract(PROFILER_CONTRACT);
+    public Contract getContract(Gateway gateway, String contract) {
+        return gateway.getNetwork(PROFILER_CHANNEL).getContract(contract);
     }
 
     public Gateway getGateway() throws IOException {
@@ -36,5 +42,55 @@ public class HyperledgerService {
                 .networkConfig(networkConfigPath)
                 .discovery(true)
                 .connect();
+    }
+
+    public Profile registerProfile(int minAge, int maxAge, String gender, int minShiftHours, int maxShiftHours,
+                                   List<String> shiftTypes, List<Integer> routeIds) throws IOException,
+            ContractException, InterruptedException, TimeoutException {
+        Gateway gateway = this.getGateway();
+        Contract contract = this.getContract(gateway, PROFILER_CONTRACT);
+
+        List<Integer> ageRange = new ArrayList<>();
+        ageRange.add(minAge);
+        ageRange.add(maxAge);
+
+        List<Integer> shiftHoursRange = new ArrayList<>();
+        shiftHoursRange.add(minShiftHours);
+        shiftHoursRange.add(maxShiftHours);
+
+        String uuid = String.valueOf(UUID.randomUUID());
+
+        Profile profile = new Profile(
+                uuid,
+                ageRange,
+                gender,
+                shiftHoursRange,
+                shiftTypes,
+                routeIds
+        );
+
+        contract.submitTransaction("CreateProfile", uuid, this.genson.serialize(profile));
+        gateway.close();
+
+        return profile;
+    }
+
+    public List<Profile> getProfiles() throws IOException, ContractException {
+        Gateway gateway = this.getGateway();
+        Contract contract = this.getContract(gateway, PROFILER_CONTRACT);
+
+        byte[] result = contract.evaluateTransaction("GetProfiles");
+        gateway.close();
+
+        return this.genson.deserialize(result, new GenericType<List<Profile>>() {});
+    }
+
+    public void setProfile(String username, String profileId) throws IOException,
+            ContractException, InterruptedException, TimeoutException {
+        Gateway gateway = this.getGateway();
+        Contract contract = this.getContract(gateway, REGISTRY_CONTRACT);
+
+        contract.submitTransaction("updateUserProfileId", username, profileId);
+        gateway.close();
     }
 }
