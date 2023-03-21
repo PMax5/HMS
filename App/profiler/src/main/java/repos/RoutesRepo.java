139805e430e40fcf89418a2d6985f497cf80ce4f
Pedro Max <53193337+PMax5@users.noreply.github.com>
2023-03-21 14:51:52 +0000

    feat: Implement routes repo

--- App/profiler/src/main/java/repos/RoutesRepo.java
+++ App/profiler/src/main/java/repos/RoutesRepo.java
@@ -1,4 +1,48 @@
 package repos;
 
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import static com.mongodb.client.model.Filters.eq;
+
 public class RoutesRepo {
+    private final String databaseAddress;
+    private MongoClient mongoClient;
+
+    public RoutesRepo() {
+        this.databaseAddress = System.getenv("CONFIG_DATABASE_ADDRESS");
+    }
+
+    private MongoDatabase newConnection(String database) {
+        String uri = "mongodb://" + this.databaseAddress + "/?retryWrites=true&w=majority";
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(uri))
+                .serverApi(serverApi)
+                .build();
+
+
+        this.mongoClient = MongoClients.create(settings);
+        return this.mongoClient.getDatabase(database);
+    }
+
+    public String getRoutes(String serviceId) {
+        MongoDatabase mongoDatabase = this.newConnection("routes");
+        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("configs");
+
+        Document document = mongoCollection.find(eq("serviceId", serviceId)).first();
+        this.mongoClient.close();
+
+        return document != null ? document.getString("serviceConfig") : "{}";
+    }
 }
