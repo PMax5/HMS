1262b0ea15face781479b152d5974ccb8673410a
Pedro Max <53193337+PMax5@users.noreply.github.com>
2023-03-30 14:16:02 +0100

    feat: Implement smart contract to save average shift statistics

--- Contracts/shiftsdata/src/main/java/ShiftData.java
+++ Contracts/shiftsdata/src/main/java/ShiftData.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class ShiftData {
+import com.owlike.genson.Genson;
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.*;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
+import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Contract(
+        name = "shiftdata",
+        info = @Info(
+                title = "Shift Data Contract",
+                description = "HMS shift data contract",
+                version = "1.0",
+                contact = @Contact(
+                        email = "servers@pedromax.pt",
+                        name = "HMS"
+                )
+        )
+)
+@Default
+public final class ShiftData implements ContractInterface {
+
+    private final Genson genson = new Genson();
+
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public void SubmitShiftData(final Context ctx, final String shiftId, final String shiftString) {
+        ChaincodeStub stub = ctx.getStub();
+        stub.putStringState(shiftId, shiftString);
+    }
+
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public ShiftDataLog[] GetShiftDataLogs(final Context ctx, final String userId) {
+        ChaincodeStub stub = ctx.getStub();
+
+        String query = String.format("{ \"selector\": { \"userId\": \"%s\" } }", userId);
+        QueryResultsIterator<KeyValue> results = stub.getQueryResult(query);
+
+        List<ShiftDataLog> queryResults = new ArrayList<>();
+
+        System.out.println("Fetching results...");
+        for (KeyValue result: results) {
+            ShiftDataLog shiftDataLog = genson.deserialize(result.getStringValue(), ShiftDataLog.class);
+            System.out.println("Result for shift log: " + shiftDataLog.getShiftId());
+            queryResults.add(shiftDataLog);
+        }
+
+        return queryResults.toArray(new ShiftDataLog[0]);
+    }
 }
--- Contracts/shiftsdata/src/main/java/ShiftDataLog.java
+++ Contracts/shiftsdata/src/main/java/ShiftDataLog.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class ShiftDataLog {
+import com.owlike.genson.annotation.JsonProperty;
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+
+import java.util.Objects;
+
+@DataType()
+public class ShiftDataLog {
+
+    @Property()
+    private final String userId;
+
+    @Property()
+    private final String shiftId;
+
+    @Property()
+    private final int vehicleId;
+
+    @Property()
+    private final int routeId;
+
+    @Property()
+    private final int averageBPM;
+
+    @Property()
+    private final int averageDrowsiness;
+
+    @Property()
+    private final int averageSpeed;
+
+    public ShiftDataLog(@JsonProperty("userId") String userId, @JsonProperty("shiftId") String shiftId,
+                    @JsonProperty("vehicleId") int vehicleId, @JsonProperty("routeId") int routeId,
+                    @JsonProperty("averageBPM") int averageBPM,
+                    @JsonProperty("averageDrowsiness") int averageDrowsiness,
+                    @JsonProperty("averageSpeed") int averageSpeed) {
+        this.userId = userId;
+        this.shiftId = shiftId;
+        this.vehicleId = vehicleId;
+        this.routeId = routeId;
+        this.averageBPM = averageBPM;
+        this.averageDrowsiness = averageDrowsiness;
+        this.averageSpeed = averageSpeed;
+    }
+
+    public String getUserId() { return this.userId; }
+
+    public String getShiftId() { return this.shiftId; }
+
+    public int getVehicleId() { return this.vehicleId; }
+
+    public int getRouteId() { return this.routeId; }
+
+    public int getAverageBPM() { return this.averageBPM; }
+
+    public int getAverageDrowsiness() { return this.averageDrowsiness; }
+
+    public int getAverageSpeed() { return this.averageSpeed; }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                this.getUserId(),
+                this.getShiftId(),
+                this.getVehicleId(),
+                this.getRouteId(),
+                this.getAverageBPM(),
+                this.getAverageDrowsiness(),
+                this.getAverageSpeed()
+        );
+    }
 }
