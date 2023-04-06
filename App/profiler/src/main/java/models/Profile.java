6860ca44ef346f0198a96cfc66c513a8d435b77a
Pedro Max <53193337+PMax5@users.noreply.github.com>
2023-04-06 15:30:58 +0100

    feat: Add route characteristics to profile model

--- App/profiler/src/main/java/models/Profile.java
+++ App/profiler/src/main/java/models/Profile.java
@@ -13,12 +13,14 @@ public class Profile {
     private List<Integer> shiftHoursRange;
     private List<ShiftType> shiftTypes;
     private List<Integer> routeIds;
+    private List<RouteCharacteristic> routeCharacteristics;
 
     public Profile(@JsonProperty("id") String id, @JsonProperty("ageRange") List<Integer> ageRange,
                    @JsonProperty("gender") String gender,
                    @JsonProperty("shiftHoursRage") List<Integer> shiftHoursRange,
                    @JsonProperty("shiftTypes") List<String> shiftTypes,
-                   @JsonProperty("routeIds") List<Integer> routeIds) {
+                   @JsonProperty("routeIds") List<Integer> routeIds,
+                   @JsonProperty("routeCharacteristics") List<String> routeCharacteristics) {
         this.id = id;
         this.ageRange = ageRange;
         this.gender = Gender.valueOf(gender);
@@ -27,6 +29,10 @@ public class Profile {
         shiftTypes.forEach((shiftType) -> {
             this.shiftTypes.add(ShiftType.valueOf(shiftType));
         });
+        routeCharacteristics.forEach((routeCharacteristic) -> {
+            this.routeCharacteristics.add(RouteCharacteristic.valueOf(routeCharacteristic));
+        });
+
         this.routeIds = routeIds;
     }
 
@@ -60,5 +66,9 @@ public class Profile {
 
     public List<Integer> getRouteIds() {
         return this.routeIds;
+    }
+
+    public List<RouteCharacteristic> getRouteCharacteristics() {
+        return this.routeCharacteristics;
     }
 }
