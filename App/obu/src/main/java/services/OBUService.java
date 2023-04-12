48138659f94e8e1d0eb553ab0eff3b04547ec737
Pedro Max <53193337+PMax5@users.noreply.github.com>
2023-04-12 14:40:21 +0100

    feat: Create OBU service skeleton

--- App/obu/src/main/java/services/OBUService.java
+++ App/obu/src/main/java/services/OBUService.java
@@ -1,2 +1,23 @@
-package services;public class OBUService {
+package services;
+
+import hmsProto.GatewaysGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class OBUService implements AutoCloseable {
+    private final GatewaysGrpc.GatewaysBlockingStub stub;
+    private final ManagedChannel channel;
+
+    public OBUService(String host, int port) {
+        this.channel = ManagedChannelBuilder
+                .forAddress(host, port)
+                .usePlaintext()
+                .build();
+        this.stub = GatewaysGrpc.newBlockingStub(this.channel);
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.channel.shutdown();
+    }
 }
