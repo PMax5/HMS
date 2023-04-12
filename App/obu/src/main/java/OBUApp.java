4e3fa7c0d897ea9291fe025a29aa99b510b22de2
Pedro Max <53193337+PMax5@users.noreply.github.com>
2023-04-12 14:39:58 +0100

    feat: Create OBU app skeleton

--- App/obu/src/main/java/OBUApp.java
+++ App/obu/src/main/java/OBUApp.java
@@ -1,5 +1,21 @@
+import services.OBUService;
+
 public class OBUApp {
     public static void main(String[] args) {
-        // TODO: Implement this method.
+        System.out.println("On Board Unit Initializing...");
+        if (args.length < 2) {
+            System.err.println("Argument(s) missing!");
+            return;
+        }
+
+        final String host = args[0];
+        final int port = Integer.parseInt(args[1]);
+        OBUService obuService = new OBUService(host, port);
+
+        try {
+            obuService.close();
+        } catch (Exception e) {
+            System.err.println("[OBU Service] Failed to shutdown OBU service: " + e.getMessage());
+        }
     }
 }
