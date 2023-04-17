52adf1711521c358e55724b064f4dd5becb64ff7
Pedro Max <53193337+PMax5@users.noreply.github.com>
2023-04-17 14:36:54 +0100

    feat: Initialize data lists

--- App/obu/src/main/java/services/OBUService.java
+++ App/obu/src/main/java/services/OBUService.java
@@ -7,9 +7,11 @@ import hmsProto.GatewaysGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OBUService implements AutoCloseable {
+
     private final GatewaysGrpc.GatewaysBlockingStub stub;
     private final ManagedChannel channel;
     private String userToken;
@@ -17,12 +19,20 @@ public class OBUService implements AutoCloseable {
     private String shiftId;
     private int routeId;
     private int vehicleId;
+    private List<Integer> bpm;
+    private List<Integer> drowsiness;
+    private List<Integer> speeds;
+    private List<Long> timestamps;
 
     public OBUService(String host, int port) {
         this.channel = ManagedChannelBuilder.forAddress(host, port)
                 .usePlaintext()
                 .build();
         this.stub = GatewaysGrpc.newBlockingStub(this.channel);
+        this.bpm = new ArrayList<>();
+        this.drowsiness = new ArrayList<>();
+        this.speeds = new ArrayList<>();
+        this.timestamps = new ArrayList<>();
     }
 
     public void loginUser(String username, String password) throws OBUException {
@@ -55,6 +65,7 @@ public class OBUService implements AutoCloseable {
         this.shiftId = null;
         this.routeId = 0;
         this.vehicleId = 0;
+        this.resetData();
     }
 
     public void startShift(int routeId, int vehicleId) throws OBUException {
@@ -87,18 +98,18 @@ public class OBUService implements AutoCloseable {
         this.shiftId = null;
         this.routeId = 0;
         this.vehicleId = 0;
+        this.resetData();
     }
 
-    public void submitUserData(List<Integer> bpms, List<Integer> drowsiness, List<Integer> speeds,
-                               List<Long> timestamps) throws OBUException {
+    public void submitUserData() throws OBUException {
         Data.DataRequest dataRequest = Data.DataRequest.newBuilder()
                 .setDriverId(this.username)
                 .setRouteId(this.routeId)
                 .setVehicleId(this.vehicleId)
-                .addAllBpm(bpms)
-                .addAllDrowsiness(drowsiness)
-                .addAllSpeeds(speeds)
-                .addAllTimestamps(timestamps)
+                .addAllBpm(this.bpm)
+                .addAllDrowsiness(this.drowsiness)
+                .addAllSpeeds(this.speeds)
+                .addAllTimestamps(this.timestamps)
                 .setShiftId(this.shiftId)
                 .build();
 
@@ -112,10 +123,35 @@ public class OBUService implements AutoCloseable {
         if (submitDataLogResponse.hasErrorMessage()) {
             throw new OBUException(submitDataLogResponse.getErrorMessage().getDescription());
         }
+
+        this.resetData();
+    }
+
+    public void addBpm(int bpm) {
+        this.bpm.add(bpm);
+    }
+
+    public void addDrowsiness(int drowsiness) {
+        this.drowsiness.add(drowsiness);
+    }
+
+    public void addSpeed(int speed) {
+        this.speeds.add(speed);
+    }
+
+    public void addTimestamp(long timestamp) {
+        this.timestamps.add(timestamp);
+    }
+
+    public void resetData() {
+        this.bpm.clear();
+        this.drowsiness.clear();
+        this.speeds.clear();
+        this.timestamps.clear();
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
         this.channel.shutdown();
     }
 }
