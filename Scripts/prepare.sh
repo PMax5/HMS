e57c7879c6e0ac95cf34283f955690cf45bd0756
Pedro Max <53193337+PMax5@users.noreply.github.com>
2023-05-26 15:25:17 +0100

    refact: Add more ports to SSH tunnel

--- Scripts/prepare.sh
+++ Scripts/prepare.sh
@@ -6,7 +6,7 @@ echo "Populating DB"
 mongo populateDB.js
 
 echo "Connecting to Hyperledger server (SSH Tunnel)"
-ssh -L 7051:localhost:7051 -L 9051:localhost:9051 -L 7054:localhost:7054 -L 8054:localhost:8054 -L 9054:localhost:9054 -L 7050:localhost:7050 ghostrider
+ssh -L 7051:localhost:7051 -L 9051:localhost:9051 -L 7054:localhost:7054 -L 8054:localhost:8054 -L 9054:localhost:9054 -L 7050:localhost:7050 -L 8080:localhost:8080 -L 27017:localhost:27017 -L 5672:localhost:5672 ghostrider
 
 #echo "Populating profiles"
 #source /root/fabric-samples/test-network/setEnv.sh
